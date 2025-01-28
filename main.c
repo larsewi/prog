0b393d08589f247f48c8d90bc6ed817498c08bb2
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-01-28 13:24:56 +0100

    Write content to immutable.txt
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- main.c
+++ main.c
@@ -1,19 +1,36 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "utils.h"
 
 int main(ARG_UNUSED int argc, ARG_UNUSED char *argv[]) {
-    printf("Hello World\n");
+  printf("Hello World\n");
 
-    const char *const filename = "immutable.txt";
+  const char content[] = "This is an immutable file";
+  const char filename[] = "immutable.txt";
 
-    int fd = open(filename, O_CREAT, O_WRONLY);
-    if (fd == -1) {
-        fprintf(stderr, "Failed to open '%s'", filename);
-        return EXIT_FAILURE;
+  int fd = open(filename, O_CREAT | O_WRONLY, (mode_t)0700);
+  if (fd == -1) {
+    fprintf(stderr, "Failed to open '%s': %s\n", filename, strerror(errno));
+    return EXIT_FAILURE;
+  }
+
+  size_t n_bytes = 0;
+  while (n_bytes < sizeof(content) - 1) {
+    ssize_t ret = write(fd, content + n_bytes,
+                        (sizeof(content) - 1 /* null-byte */) - n_bytes);
+    if (ret < 0) {
+      fprintf(stderr, "Failed to write '%s' to file '%s': %s\n", content,
+              filename, strerror(errno));
+      return EXIT_FAILURE;
     }
+    n_bytes += (size_t)ret;
+  }
 
-    return EXIT_SUCCESS;
+  close(fd);
+  return EXIT_SUCCESS;
 }
