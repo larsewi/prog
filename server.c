93ef27c4300556acab5d0a0ebb7f51e0de1cd000
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-03-03 17:04:50 +0100

    Send delta
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- server.c
+++ server.c
@@ -13,9 +13,13 @@ static char in_buf[BUFFER_SIZE * 2], out_buf[BUFFER_SIZE * 2];
 
 static int accept_connection(void);
 static int recv_signature(int sock, rs_signature_t **sig);
+static int send_delta(int sock, rs_signature_t *sig, const char *fname);
 
 int main(int argc, char *argv[])
 {
+    /* Parse arguments (use stdin if no argument) */
+    const char *fname = (argc >= 2) ? argv[1] : NULL;
+
     puts("Waiting for connection...");
     int sock = accept_connection();
     if (sock == -1) {
@@ -25,6 +29,14 @@ int main(int argc, char *argv[])
     puts("Receiving signature...");
     rs_signature_t *sig;
     int ret = recv_signature(sock, &sig);
+    if (ret == -1) {
+        close(sock);
+        return EXIT_FAILURE;
+    }
+
+    puts("Sending delta...");
+    ret = send_delta(sock, sig, fname);
+    rs_free_sumset(sig);
     if (ret == -1) {
         close(sock);
         return EXIT_FAILURE;
@@ -130,6 +142,87 @@ static int recv_signature(int sock, rs_signature_t **sig) {
         /* The job should take care of draining the buffers */
     } while (res != RS_DONE);
 
+    rs_job_free(job);
+    return 0;
+}
+
+static int send_delta(int sock, rs_signature_t *sig, const char *fname) {
+    /* Build hash table */
+    rs_result res = rs_build_hash_table(sig);
+    if (res != RS_DONE) {
+        return -1;
+    }
+
+    /* Open file up-to-date file */
+    FILE *file = rs_file_open(fname, "rb", 0);
+    assert(file != NULL);
+
+    /* Start generating delta */
+    rs_job_t *job = rs_delta_begin(sig);
+    assert(job != NULL);
+
+    /* Setup buffers */
+    rs_buffers_t bufs = { 0 };
+    bufs.next_in = in_buf;
+    bufs.next_out = out_buf;
+    bufs.avail_out = BUFFER_SIZE; /* We cannot send more in one message */
+
+    do {
+        if (bufs.eof_in == 0) {
+            if (bufs.avail_in >= sizeof(in_buf)) {
+                /* The job requires more data, but the input buffer is full */
+                fputs("Insufficient buffer capacity", stderr);
+                rs_file_close(file);
+                rs_job_free(job);
+                return -1;
+            }
+
+            if (bufs.avail_in > 0) {
+                /* Left over tail data, move to front */
+                memmove(in_buf, bufs.next_in, bufs.avail_in);
+            }
+
+            /* Fill input buffer */
+            size_t n_bytes = fread(in_buf + bufs.avail_in, 1, sizeof(in_buf) - bufs.avail_in, file);
+            if (n_bytes == 0) {
+                if (ferror(file)) {
+                    perror("Failed to read file");
+                    rs_file_close(file);
+                    rs_job_free(job);
+                    return -1;
+                }
+                bufs.eof_in = feof(file);
+            }
+
+            bufs.next_in = in_buf;
+            bufs.avail_in += n_bytes;
+        }
+
+        res = rs_job_iter(job, &bufs);
+        if (res != RS_DONE && res != RS_BLOCKED) {
+            rs_file_close(file);
+            rs_job_free(job);
+            return -1;
+        }
+
+        /* Drain output buffer, if there is data */
+        assert(bufs.next_out >= out_buf);
+        size_t present = (size_t)(bufs.next_out - out_buf);
+        if (present > 0 || res == RS_DONE) {
+            assert(present <= BUFFER_SIZE);
+            int ret = send_message(sock, out_buf, present, (res == RS_DONE) ? 1 : 0);
+            if (ret == -1) {
+                rs_file_close(file);
+                rs_job_free(job);
+                return -1;
+            }
+
+            bufs.next_out = out_buf;
+            bufs.avail_out = BUFFER_SIZE;
+        }
+    } while (res != RS_DONE);
+
+    rs_file_close(file);
     rs_job_free(job);
     return 0;
 }
