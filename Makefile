0e5811391f374c48d113f64abcf6d75517afafd8
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-03-03 13:13:38 +0100

    Send signature
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- Makefile
+++ Makefile
@@ -1,18 +1,19 @@
 CC = gcc
 CFLAGS = -g -Wall -Wextra -Wconversion
+LDFLAGS = -lrsync
 
 .PHONY: all clean
 
 all: server client
 
 server: server.o
-	$(CC) server.o -o server
+	$(CC) server.o -o server $(LDFLAGS)
 
 server.o: server.c common.h
 	$(CC) $(CFLAGS) -c server.c -o server.o
 
 client: client.o
-	$(CC) client.o -o client
+	$(CC) client.o -o client $(LDFLAGS)
 
 client.o: client.c common.h
 	$(CC) $(CFLAGS) -c client.c -o client.o
--- client.c
+++ client.c
@@ -20,12 +20,8 @@ static int connect_to_server(const char *ip_addr);
 static int send_signature(int sock, const char *fname);
 
 int main(int argc, char *argv[]) {
-    /* Parse arguments */
-    if (argc < 2) {
-        printf("USAGE: %s <FILENAME>", argv[0]);
-        return EXIT_FAILURE;
-    }
-    const char *fname = argv[1];
+    /* Parse arguments (use stdin and stdout if no argument) */
+    const char *fname = (argc >= 2) ? argv[1] : NULL;
 
     puts("Connecting to server...");
     int sock = connect_to_server(IP_ADDRESS);
@@ -67,4 +63,104 @@ static int connect_to_server(const char *ip_addr) {
     }
 
     return sock;
+}
+
+static int send_signature(int sock, const char *fname) {
+    /* Make sure the basis file exists, unless it is stdin */
+    const int use_io_stream = (fname == NULL) || (strcmp(fname, "-") == 0);
+    if (!use_io_stream) {
+        FILE *file = rs_file_open(fname, "a", 0);
+        if (file != NULL) {
+            rs_file_close(file);
+        }
+    }
+
+    /* Open basis file */
+    FILE *file = rs_file_open(fname, "rb", 0);
+    assert(file != NULL);
+
+    /* Get file size */
+    rs_long_t fsize = rs_file_size(file);
+
+    /* Get recommended arguments */
+    rs_magic_number sig_magic = 0;
+    size_t block_len = 0, strong_len = 0;
+    rs_result res = rs_sig_args(fsize, &sig_magic, &block_len, &strong_len);
+    if (res != RS_DONE) {
+        rs_file_close(file);
+        return -1;
+    }
+
+    /* Start generating signature */
+    rs_job_t *job = rs_sig_begin(block_len, strong_len, sig_magic);
+    assert(job != NULL);
+
+    /* Setup buffers */
+    rs_buffers_t bufs = { 0 };
+    bufs.next_in = in_buf;
+    bufs.next_out = out_buf;
+    bufs.avail_out = BUFFER_SIZE; /* We cannot send more in one message */
+
+    /* Generate signature */
+    do {
+        if (bufs.eof_in == 0) {
+            if (bufs.avail_in >= sizeof(in_buf)) {
+                /* The job requires more data, but the input buffer is full */
+                fputs("Insufficient buffer capacity", stderr);
+                rs_file_close(file);
+                rs_job_free(job);
+                return -1;
+            }
+
+            if (bufs.avail_in > 0) {
+                /* Leftover tail data, move it to front */
+                memmove(in_buf, bufs.next_in, bufs.avail_in);
+            }
+
+            /* Fill input buffer */
+            size_t n_bytes = fread(in_buf + bufs.avail_in, 1, sizeof(in_buf) - bufs.avail_in, file);
+            if (n_bytes == 0) {
+                if (ferror(file)) {
+                    perror("Failed to read file");
+                    rs_file_close(file);
+                    rs_job_free(job);
+                    return -1;
+                }
+
+                /* End-of-File reached */
+                bufs.eof_in = feof(file);
+                assert(bufs.eof_in != 0);
+            }
+
+            bufs.next_in = in_buf;
+            bufs.avail_in += n_bytes;
+        }
+
+        /* Iterate job */
+        res = rs_job_iter(job, &bufs);
+        if (res != RS_DONE && res != RS_BLOCKED) {
+            rs_file_close(file);
+            rs_job_free(job);
+            return -1;
+        }
+
+        size_t present = bufs.next_out - out_buf;
+        if (present > 0 || res == RS_DONE) {
+            /* Drain output buffer */
+            assert(present <= BUFFER_SIZE);
+            int ret = send_message(sock, out_buf, present, (res == RS_DONE) ? 1 : 0);
+            if (ret == -1) {
+                rs_file_close(file);
+                rs_job_free(job);
+                return -1;
+            }
+
+            bufs.next_out = out_buf;
+            bufs.avail_out = BUFFER_SIZE;
+        }
+    } while (res != RS_DONE);
+
+    rs_job_free(job);
+    rs_file_close(file);
+    return 0;
 }
